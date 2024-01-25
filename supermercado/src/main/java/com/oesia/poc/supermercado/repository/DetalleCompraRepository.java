d2016cca32d98f2b75fec0d05ae8971434717dc5
ITDEUSTO\EXTdvelam <extdvelam@oesia.com>
2024-01-25 13:38:55 +0100

    no message

--- supermercado/src/main/java/com/oesia/poc/supermercado/repository/DetalleCompraRepository.java
+++ supermercado/src/main/java/com/oesia/poc/supermercado/repository/DetalleCompraRepository.java
@@ -4,10 +4,6 @@ import com.oesia.poc.supermercado.model.DetalleCompra;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DetalleCompraRepository extends JpaRepository<DetalleCompra, Integer> {
-
-    List<DetalleCompra> findByClienteId(int clienteId);
 }
