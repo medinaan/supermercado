f59a2fc1595004d7455214213db57226d88554e5
ITDEUSTO\EXTdvelam <extdvelam@oesia.com>
2024-01-25 12:57:11 +0100

    Borrado código de sobra

--- supermercado/src/main/java/com/oesia/poc/supermercado/repository/HistorialCompraRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oesia.poc.supermercado.repository;// Asegúrate de importar las clases necesarias
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface HistorialCompraRepository extends JpaRepository<HistorialCompra, Long> {
-    List<HistorialCompra> findByClienteId(String clienteId);
-    // Puedes agregar más consultas según tus necesidades
-}
